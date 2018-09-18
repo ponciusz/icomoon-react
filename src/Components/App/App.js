fcfea69f011e8048a85a48fafb60f1b7b1c1d851
Kamil Albrycht <ponciusz@gmail.com>
2018-09-18 22:58:45 +0100

    Refactor simple demo

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import IcomoonReact, { iconList } from '../IcomoonReact';
+import Icon from '../Icon';
+import { iconList } from '../IcomoonReact';
 import iconSet from '../../assets/icons/selection.json';
+
+const styles = {
+  borderCollapse: 'collapse',
+  border: '1px solid black',
+  padding: '10px',
+};
 
 const App = () => (
   <div>
-    <IcomoonReact iconSet={iconSet} color="#444" size={100} icon="bath" />
-    {console.table(iconList(iconSet))}
+    <table style={styles}>
+      <thead>
+        <tr>
+          <th style={styles} colSpan={2}>
+Icon List
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {iconList(iconSet).map(icon => (
+          <tr>
+            <td style={styles}>
+              <Icon color="#444" size={30} icon={icon} />
+            </td>
+            <td style={styles}>
+              {icon}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   </div>
 );
 
---
+++ src/Components/Icon/Icon.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import IcomoonReact from '../IcomoonReact';
+import iconSet from '../../assets/icons/selection.json';
+
+const Icon = (props) => {
+  const { color, size, icon } = props;
+  return <IcomoonReact iconSet={iconSet} color={color || '#f00'} size={size || 100} icon={icon} />;
+};
+
+Icon.propTypes = {
+  color: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  size: PropTypes.number.isRequired,
+};
+
+export default Icon;

1782bd57a122c7ebe86021d47181cb1c89749f7e
Kamil Albrycht <ponciusz@gmail.com>
2018-12-12 08:20:56 +0000

    pass className inside component

--- src/Components/Icon/Icon.js
+++ src/Components/Icon/Icon.js
@@ -4,17 +4,29 @@ import IcomoonReact from '../IcomoonReact';
 import iconSet from '../../assets/icons/selection.json';
 
 const Icon = (props) => {
-  const { color, size, icon } = props;
-  return <IcomoonReact iconSet={iconSet} color={color} size={size} icon={icon} />;
+  const {
+    color, size, icon, className,
+  } = props;
+  return (
+    <IcomoonReact
+      className={className}
+      iconSet={iconSet}
+      color={color}
+      size={size}
+      icon={icon}
+    />
+  );
 };
 
 Icon.propTypes = {
+  className: PropTypes.string,
   color: PropTypes.string,
   icon: PropTypes.string.isRequired,
   size: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 Icon.defaultProps = {
+  className: '',
   color: '#f00',
   size: '100%',
 };

7af02892b508d2cfc935a7f95d62838d4d131dce
Kamil Albrycht <ponciusz@gmail.com>
2019-12-07 18:26:25 +0000

    fix coverage

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,6 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest"
   },
   collectCoverage: true,
-  collectCoverageFrom: ["**/*.{js,jsx,ts,tsx}"],
+  collectCoverageFrom: ["**/*.{js,jsx,ts,tsx}", "!src/index.ts"],
   verbose: false
 };
